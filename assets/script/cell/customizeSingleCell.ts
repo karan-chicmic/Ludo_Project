e56be5703eb981802e753f91a8a581d6c982d239
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-05-13 12:18:52 +0530

    board cell same size , ladder size bug fixed

--- assets/script/cell/customizeSingleCell.ts
+++ assets/script/cell/customizeSingleCell.ts
@@ -1,4 +1,17 @@
-import { _decorator, Component, Label, math, Node, Sprite, SpriteFrame, UITransform, view } from "cc";
+import {
+    _decorator,
+    color,
+    Color,
+    Component,
+    Label,
+    math,
+    Node,
+    Sprite,
+    SpriteFrame,
+    UIStaticBatch,
+    UITransform,
+    view,
+} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("customizeSingleCell")
@@ -21,8 +34,13 @@ export class customizeSingleCell extends Component {
     update(deltaTime: number) {}
     setLable(count: number) {
         this.designResolution = view.getDesignResolutionSize();
+        let cellHeight = (this.designResolution.height * 80) / 1000;
+        let cellWidth = (this.designResolution.width * 80) / 1000;
+        this.node.getComponent(UITransform).height = cellHeight;
+        this.node.getComponent(UITransform).width = cellWidth;
 
         this.label.string = count.toString();
+        this.label.color = count % 2 == 0 ? Color.BLUE : Color.YELLOW;
 
         this.image.spriteFrame = count % 2 == 0 ? this.yellow : this.blue;
     }
